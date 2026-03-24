d6e90584524fb006e61042e299af917bcd0ce9a0
unknown <galuhsaga228@gmail.com>
2026-03-25 02:24:08 +0700

    routes dan boostrap

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\Api\WashNoteController;
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('/login', [AuthController::class, 'login']);
+
         Route::middleware('auth:sanctum')->group(function () {
             Route::get('/me', [AuthController::class, 'me']);
             Route::post('/logout', [AuthController::class, 'logout']);
