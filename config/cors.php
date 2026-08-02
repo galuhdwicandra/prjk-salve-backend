2bf2d0f51880f4bed320d0a34e3df1abf5ee44c2
unknown <galuhsaga228@gmail.com>
2026-08-02 12:29:55 +0700

    perbaikan cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(array_map(
-        'trim',
-        explode(',', (string) env('FRONTEND_URL', 'http://localhost:5173'))
-    )),
+    'allowed_origins' => array_values(array_filter(array_map('trim', explode(
+        ',',
+        (string) env('CORS_ALLOWED_ORIGINS', env('FRONTEND_URL', 'http://localhost:5173'))
+    )))),
 
     'allowed_origins_patterns' => [],
 
